4572fd38954015f8c2fb891ee21f5b13137a8729
DJaegerScript <adjiedjaka0902@gmail.com>
2024-12-15 10:01:46 +0700

    fix shell file

--- configs/migrate.sh
+++ configs/migrate.sh
@@ -10,11 +10,13 @@ do
 done
 
 SQL_QUERY=$(cat ./migrations.sql)
-if [[ -z "$SQL_QUERY" ]]; then
+if [ -z "$SQL_QUERY" ]; then
     echo "Error: Unable to read the SQL file or the file is empty."
 fi
-docker exec -i "new-eews-postgres" sh -c "PGPASSWORD=$DB_PASSWORD psql -U $DB_USER -d postgres" <<< "$SQL_QUERY"
-if [[ $? -eq 0 ]]; then
+
+echo "$SQL_QUERY" | docker exec -i "new-eews-postgres" sh -c "PGPASSWORD=$DB_PASSWORD psql -U $DB_USER -d postgres"
+
+if [ $? -eq 0 ]; then
     echo "SQL query executed successfully inside the Docker container."
 else
     echo "Error: Failed to execute the SQL query inside the Docker container."
