f8123d547f6c08175d0e5c7f7175ff20b61d43e6
DJaegerScript <adjiedjaka0902@gmail.com>
2024-12-16 16:44:17 +0700

    add stations table

--- configs/migrations.sql
+++ configs/migrations.sql
@@ -6,6 +6,30 @@ CREATE TABLE IF NOT EXISTS configurations (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Add a trigger to the `configurations` table
+CREATE TRIGGER set_updated_at
+BEFORE UPDATE ON configurations
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
+CREATE TABLE IF NOT EXISTS stations {
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name VARCHAR(255) NOT NULL,
+    code VARCHAR(255) NOT NULL UNIQUE,
+    lat DECIMAL NOT NULL,
+    long DECIMAL NOT NULL,
+    is_enabled BOOLEAN DEFAULT TRUE,
+    nearest_stations VARCHAR(255)[] NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+}
+
+-- Add a trigger to the `configurations` table
+CREATE TRIGGER set_updated_at
+BEFORE UPDATE ON stations
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
 -- Add a trigger function to update the `updated_at` column
 CREATE OR REPLACE FUNCTION update_updated_at_column()
 RETURNS TRIGGER AS $$
@@ -15,8 +39,4 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Add a trigger to the `configurations` table
-CREATE TRIGGER set_updated_at
-BEFORE UPDATE ON configurations
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
+
