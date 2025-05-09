65eb10526cfc03281b1d4ecef5fa9adc59c5f73f
DJaegerScript <adjiedjaka0902@gmail.com>
2025-05-09 19:03:58 +0700

    add deployment configuration for machine learning service and Docker setup

--- machine-learning/manage.py
+++ machine-learning/manage.py
@@ -1,8 +1,9 @@
 from app import create_app, db
 from flask_migrate import Migrate
+from asgiref.wsgi import WsgiToAsgi
 
 app = create_app()
 migrate = Migrate(app, db)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+asgi_app = WsgiToAsgi(app)
+
