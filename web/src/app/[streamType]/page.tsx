a0ca9c659b60c64e50e648bb6e08aac31a8cb751
DJaegerScript <adjiedjaka0902@gmail.com>
2025-06-12 00:20:11 +0700

    attempt to do dynamic map import

--- web/src/app/[streamType]/page.tsx
+++ web/src/app/[streamType]/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 
-import MapContainer from "../../modules/common/components/Map";
+import dynamic from "next/dynamic";
+
+const DynamicMap = dynamic(() => import("../../modules/common/components/Map"), {
+    ssr: false,
+})
 import {getStations} from "@/modules/station/actions/getStations";
 import StationList from "@/modules/station/components/StationList";
 import StationInfo from "@/modules/station/components/StationInfo";
 import WaveFormChart from "@/modules/waveForm/components/WaveFormChart";
 import {useQuery} from "@tanstack/react-query";
 
-export default function StreamPage() {
+export default function Page() {
     const {data: stations, isLoading} = useQuery({
         queryKey: ['stations'],
         queryFn: () => getStations(),
@@ -18,7 +22,7 @@ export default function StreamPage() {
             <div className={'w-full flex flex-col gap-4 h-full'}>
                 <div className={'w-full grid grid-cols-12 gap-4 row-span-5 h-full'}>
                     <div className={'col-span-9 h-full bg-gray-600/50 rounded-2xl p-2'}>
-                        <MapContainer stations={stations}/>
+                        <DynamicMap stations={stations}/>
                     </div>
                     <div className={'col-span-3 flex flex-col h-full gap-3'}>
                         <div className={'rounded-2xl bg-gray-600/50 flex justify-center items-center'}>
